2871622d8b38a335ba5247134d233009f661faa0
Yulian302 <bohomolyulian3022003@gmail.com>
2024-03-05 16:50:46 -0500

    Menu serializer

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("items/", views.MenuItemsView.as_view(), name="menu-items"),
+    path("items/<int:pk>/", views.SingleMenuItemView.as_view(), name="menu-item"),
+]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import *
+from .serializers import *
 
 
 def index(request):
     return render(request, "index.html", context={})
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
