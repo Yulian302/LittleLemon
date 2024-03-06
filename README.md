92a626181ec21b222293a1db38282b4fd5ac2689
Yulian302 <bohomolyulian3022003@gmail.com>
2024-03-06 13:09:52 -0500

    final corrections applied

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # LittleLemon
 
 Little Lemon Capstone Project (Meta Course)
+
+**Api to test:**
+
+- restaurant/booking/
+- restaurant/menu-items/
+- restaurant/menu-items/{menuItemId}/
+- auth/token/login/
+- auth/users/
+- auth/users/me/
+- ...
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("items/", views.MenuItemsView.as_view(), name="menu-items"),
-    path("items/<int:pk>/", views.SingleMenuItemView.as_view(), name="menu-item"),
+    path("menu-items/", views.MenuItemsView.as_view(), name="menu-items"),
+    path("menu-items/<int:pk>/", views.SingleMenuItemView.as_view(), name="menu-item"),
 ]
